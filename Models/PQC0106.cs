87cd770c39786e9438aedbce6d7a2d0a6e4d3ef8
PhuongQuocChien <chienquocphuong@gmail.com>
2023-01-04 08:48:01 +0700

    Create_Table_PQC106

--- Models/PQC0106.cs
+++ Models/PQC0106.cs
@@ -4,8 +4,11 @@ namespace PhuonQuocChien106.Models
     public class PQC0106
     {
         [Key]
+        [Display (Name ="Mã")]
         public string? PQCId {get; set;} 
+        [Display (Name ="Tên")]
         public string? PQCName {get; set;}
+        [Display (Name ="Giới tính")]
         public string? PQCGender {get; set;}
         
 }
--- Models/UniversityPQC106.cs
+++ Models/UniversityPQC106.cs
@@ -4,7 +4,9 @@ namespace PhuonQuocChien106.Models
     public class UniversityPQC106
     {
         [Key]
+        [Display (Name ="Mã")]
         public string? UniversityId {get; set;} 
+        [Display (Name ="Tên")]
         public string? UniversityName {get; set;}
         
 }
